1fc9638c65c59d883ee42eedcd1e380e5cafd356
softyAmir <theyabcpssouaf2@gmail.com>
2023-07-24 05:18:05 -1100

    commit

--- Backend/db/connect.js
+++ Backend/db/connect.js
@@ -1,9 +1,3 @@
 const mongoose = require('mongoose');
-
-const connectdb = async (url)=>{
-    await mongoose.connect(url).then(
-        ()=>console.log('mongodb connected.../')
-    ).catch(err=>console.log(err))
-}   
-
+const connectdb = async (url)=>await mongoose.connect(url).then(()=>console.log('mongodb connected.../')).catch(err=>console.log(err))
 module.exports = connectdb;
