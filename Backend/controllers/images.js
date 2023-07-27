49b89fb8436593022c973f207ebaa8da5de6af86
softyAmir <theyabcpssouaf2@gmail.com>
2023-07-27 09:25:13 -1100

    openAi commit config

--- Backend/controllers/images.js
+++ Backend/controllers/images.js
@@ -1,31 +1,30 @@
-const {Configuration, OpenAIApi} = require('openai');
-require('dotenv').config();
+// const {Configuration, OpenAIApi} = require('openai');
+// require('dotenv').config();
 
-const configuration = new Configuration({
-    apiKey: process.env.openAiKey,
-});
+// const configuration = new Configuration({
+//     apiKey: process.env.openAiKey,
+// });
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
 
-const getImage = async (req, res) => {
-    try {
-      const { prompt } = req.body;
-      const aiResponse = await openai.createImage({
-        prompt,
-        n: 1,
-        size: '1024x1024',
-        response_format: 'b64_json',
-      });
-  
-      const image = aiResponse.data.data[0].b64_json;
-      res.status(200).json({ photo: image });   
-    } catch (error) {
-      res.status(500).send(error?.response.data.error.message || 'Something went wrong');
-    }
-};
+// const getImage = async (req, res) => {
+//     try {
+//       const { prompt } = req.body;
+//       const aiResponse = await openai.createImage({
+//         prompt,
+//         n: 1,
+//         size: '1024x1024',
+//         response_format: 'b64_json',
+//       });
 
-module.exports = {
-    getImage
-}
-  
+//       const image = aiResponse.data.data[0].b64_json;
+//       res.status(200).json({ photo: image });
+//     } catch (error) {
+//       res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+//     }
+// // };
+
+// module.exports = {
+//     getImage
+// }
